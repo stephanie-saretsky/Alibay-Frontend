4062feb50763ad89186606451c9c6a48ab871f97
Stephanie Saretsky <stephanie@Stephanies-MacBook-Pro-2.local>
2019-05-06 19:12:53 -0400

    teadetails

---
+++ Frontend/src/CoffeeDetails.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Coffee from "./Coffee.jsx";
+
+class UnconnectedCoffeeDetails extends Component {}

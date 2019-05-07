d420f206d8e3f8b7e15021912273942a99f6fd90
Lucifer <lucile.garnaud91@gmail.com>
2019-05-07 10:41:56 -0400

    try11

--- Frontend/src/Money.jsx
+++ Frontend/src/Money.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-export default class Money extends React.Component {
-  onToken = (token) => {
-    fetch('/save-stripe-token', {
-      method: 'POST',
-      body: JSON.stringify(token),
+class Money extends Component {
+  onToken = token => {
+    fetch("/save-stripe-token", {
+      method: "POST",
+      body: JSON.stringify(token)
     }).then(response => {
       response.json().then(data => {
         alert(`We are in business, ${data.email}`);
       });
     });
-  }
+  };
 
   // ...
 
@@ -23,8 +23,8 @@ export default class Money extends React.Component {
         stripeKey="	
         sk_test_x5gHDTvOcukvjpirrZwgbx5X00ovsBO0zU"
       />
-    )
+    );
   }
 }
 
-export default Money
+export default Money;

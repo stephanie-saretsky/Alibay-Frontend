73048d51178505a396ae05c8bdc8408ab1c12a28
Stephanie Saretsky <stephanie@Stephanies-MacBook-Pro-2.local>
2019-05-07 11:00:27 -0400

    stripe change

---
+++ Frontend/src/Stripe.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+class Stripe extends Component {
+  onToken = token => {
+    fetch("/save-stripe-token", {
+      method: "POST",
+      body: JSON.stringify(token)
+    }).then(response => {
+      response.json().then(data => {
+        alert(`We are in business, ${data.email}`);
+      });
+    });
+  };
+
+  // ...
+
+  render() {
+    return (
+      // ...
+      <StripeCheckout
+        token={this.onToken}
+        stripeKey="	
+        pk_test_o0jp2CyctV96lKqFAIdFE4i0008Y2G9odT"
+      />
+    );
+  }
+}
+
+export default Stripe;

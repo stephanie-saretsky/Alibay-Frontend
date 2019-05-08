c4516244a17ab71a589afc130be01738bfd7ba99
Lucifer <lucile.garnaud91@gmail.com>
2019-05-08 12:14:08 -0400

    push my frontend

--- Frontend/src/Review.jsx
+++ Frontend/src/Review.jsx
@@ -3,6 +3,14 @@ import "./main.css";
 import { Link } from "react-router-dom";
 
 class Review extends Component {
+  // renderRating = props => {
+  //   let stars = [];
+  //   for (let i = 0; i < props.rating; i++) {
+  //     stars = stars.concat("★");
+  //   }
+  //   return stars;
+  // };
+
   render() {
     return (
       <div>

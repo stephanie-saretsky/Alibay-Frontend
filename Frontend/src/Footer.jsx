8c22ae05f81a4bf460548175649afd857f8acf27
Lucifer <lucile.garnaud91@gmail.com>
2019-05-09 13:30:11 -0400

    nice pull4

--- Frontend/src/Footer.jsx
+++ Frontend/src/Footer.jsx
@@ -6,23 +6,27 @@ import "./footer.css";
 class Footer extends Component {
   render = () => {
     return (
-      <footer id="footer flex">
-        <div className="image">
-          <img src="/logo.png" height="40px" />
-        </div>
-        <p>
-          <strong>Contact</strong>
-        </p>
+      <footer id="footer" className="flex">
         <div>
-          <a
-            href={"http://www.facebook.com/pages/Rishi-Tea/39035256828"}
-            target={"_blank"}
-          >
-            <img src={"/facebook.png"} height="20px" />
-          </a>
-          <a href={"http://twitter.com/RishiTea"} target={"_blank"}>
-            <img src={"/twitter.png"} height="20px" />
-          </a>
+          <p>Contact</p>
+        </div>
+        <div className="image">
+          <img src="/logo.png" height="50px" />
+        </div>
+        <div className="flex">
+          <div>
+            <a
+              href={"http://www.facebook.com/pages/Rishi-Tea/39035256828"}
+              target={"_blank"}
+            >
+              <img src={"/facebook.png"} height="20px" />
+            </a>
+          </div>
+          <div>
+            <a href={"http://twitter.com/RishiTea"} target={"_blank"}>
+              <img src={"/twitter.png"} height="20px" />
+            </a>
+          </div>
         </div>
       </footer>
     );
